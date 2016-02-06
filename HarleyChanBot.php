7a2995ed179e922e00e72ed04ba70e6565fbd273
enechan <karateka.black.belt@gmail.com>
2016-02-06 02:02:42 -0300

    Create HarleyChanBot.php

---
+++ HarleyChanBot.php
@@ -0,0 +1,10 @@
+<?php
+
+$botToken = "184412293:AAFCeyxkxgK46AAwmm4hD8HhP2AY9QpvvxY";
+$website = "https://api.telegram.org/bot".$botToken;
+
+$update = file_get_contents($website."/getupdates");
+
+print_r($update);
+
+?>
